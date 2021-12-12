fbe299c2d3a4d83d2340d2bb92d0dd91b79dc444
Alina <amanbekovaalina20@gmail.com>
2021-12-12 19:00:13 +0600

    5 commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from.forms import *
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.views import generic
+ 
 # Create your views here.
 def home(request):
      return render(request,'dashboard/home.html')          
@@ -20,3 +22,6 @@ def notes(request):
 def delete_note(request,pk=None):   
      Notes.objects.get(id=pk).delete()
      return redirect("notes")
+
+class NotesDetailView(generic.DetailView):
+     model=Notes 
